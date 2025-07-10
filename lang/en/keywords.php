6edfc3640f80b1bbfc046405ca6416f488e13ac0
Brahmia Lokman Abdelmoname <brahmia.lokmeneabdelmoname@univ-guelma.dz>
2025-07-10 10:58:31 +0100

    add dashboard localization

---
+++ lang/en/keywords.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+   
+
+    'dashboard' => 'Dashboard',
+    'index' => 'Index',
+    'welcome' => 'Welcome!',
+
+    // Login page translations
+    'sign_in'  => 'Sign In',
+    'email'    => 'Email',
+    'password' => 'Password',
+    'remember' => 'Remember Me',
+    'login'    => 'Login',
+    'logout'   => 'Logout',
+    
+];
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use \Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,18 +15,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::name('front.')->group(function() {
+    Route::view('/','front.index')->name('index');
+    Route::view('/about','front.about')->name('about');
+    Route::view('/service','front.service')->name('service');
+    Route::view('/contact','front.contact')->name('contact');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::name('admin.')->prefix(LaravelLocalization::setLocale() . '/admin')->middleware([ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ])->group(function () {
+    Route::middleware('auth')->group(function() {
+        Route::view('/','admin.index')->name('index');
+    });
+    Route::view('/login','admin.auth.login')->name('login');
+    require __DIR__.'/auth.php';
 });
 
-require __DIR__.'/auth.php';
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+
